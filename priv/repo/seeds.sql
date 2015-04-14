4b71ad1757a3e15547eb76bc87e83218a5f296d9
Arjan van der Gaag <arjan@arjanvandergaag.nl>
2015-04-14 20:09:23 +0200

    Add seed data

---
+++ priv/repo/seeds.sql
@@ -0,0 +1,2 @@
+INSERT INTO projects (title, settings) VALUES ('Project 1', '{ "heroku_app": "myapp" }'), ('Project 2', '{}'), ('Project 3', '{}');
+INSERT INTO badges (label, value, project_id) VALUES ('Deployment', '2015-04-15 12:00:00', 1);
