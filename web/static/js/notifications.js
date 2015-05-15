eb31817959ae8d2e20c3190c487a9d69c058c386
Arjan van der Gaag <arjan@arjanvandergaag.nl>
2015-05-15 08:47:40 +0200

    Adapt to new Phoenix.Socket

--- web/static/js/notifications.js
+++ web/static/js/notifications.js
@@ -3,7 +3,9 @@ import {Socket} from "../vendor/phoenix";
 export default function(cb) {
   let socket = new Socket('/ws');
   socket.connect();
-  socket.join('notifications', {}).receive('ok', chan => {
-    chan.on('project:update', cb);
-  });
+  let channel = socket.chan('notifications', {});
+  channel.on('project:update', cb);
+  channel.join().receive('ok', chan => {
+    console.log('Subscribed to wall updates');
+  };
 }
