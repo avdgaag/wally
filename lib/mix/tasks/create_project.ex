a8727fb0accde27d22ba45139e977230b91d8d7b
Arjan van der Gaag <arjan@arjanvandergaag.nl>
2016-01-08 07:11:58 +0100

    Refactor create_project task

--- lib/mix/tasks/create_project.ex
+++ lib/mix/tasks/create_project.ex
@@ -9,26 +9,27 @@ defmodule Mix.Tasks.Wally.CreateProject do
   """
 
   def run([name | _args]) do
-    token = :crypto.strong_rand_bytes(32)
-    |> Base.url_encode64
-    |> binary_part(0, 32)
-    project = %Wally.Project{
-      name: name,
-      api_token: token
-    }
-    Wally.Repo.persist(project, [:api_token])
+    {:ok, project } =
+      %Wally.Project{name: name, api_token: random_token(32)}
+      |> Wally.Repo.persist([:api_token])
     Mix.shell.info """
-    Created project #{name} with API token #{token}.
+    Created project #{project.name} with API token #{project.api_token}.
 
     Use URLs like this to post events:
 
-      http://domain.tld/api/hooks/#{token}/heroku
-      http://domain.tld/api/hooks/#{token}/errbit
-      http://domain.tld/api/hooks/#{token}/github
+      http://domain.tld/api/hooks/#{project.api_token}/heroku
+      http://domain.tld/api/hooks/#{project.api_token}/errbit
+      http://domain.tld/api/hooks/#{project.api_token}/github
     """
   end
 
   def run([]) do
     Mix.shell.error "No project name provided. Usage: mix wally.create_project my_project"
+  end
+
+  defp random_token(length) do
+    :crypto.strong_rand_bytes(length)
+    |> Base.url_encode64
+    |> binary_part(0, length)
   end
 end
