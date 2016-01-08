464403576725fb346c12c396fc32cf982361fdf2
Arjan van der Gaag <arjan@arjanvandergaag.nl>
2016-01-08 07:12:45 +0100

    Remove unused Project.heroku_app key

--- web/models/project.ex
+++ web/models/project.ex
@@ -3,5 +3,5 @@ defmodule Wally.Project do
   Represents a project on the status wall that has a status for various aspects.
   """
 
-  defstruct id: nil, name: nil, heroku_app: nil, api_token: nil
+  defstruct id: nil, name: nil, api_token: nil
 end
